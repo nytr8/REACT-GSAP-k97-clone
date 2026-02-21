7592d98b93e7d455e49249cbf62c45d679b94eed
Srutidev Barman <srutidevbarman2002@gmail.com>
2026-02-21 22:40:18 +0530

    aded page transition component

---
+++ src/components/common/Stairs.jsx
@@ -0,0 +1,62 @@
+import { useRef } from "react";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import { useLocation } from "react-router-dom";
+
+const Stairs = ({ children }) => {
+  const stairParent = useRef(null);
+  const pageRef = useRef(null);
+  const currentLocation = useLocation().pathname;
+  console.log(currentLocation);
+  useGSAP(() => {
+    const tl = gsap.timeline();
+    tl.to(stairParent.current, {
+      display: "block",
+    });
+    tl.from(".stairs", {
+      height: 0,
+      stagger: {
+        amount: -0.25,
+      },
+    });
+    tl.to(".stairs", {
+      y: "100%",
+      stagger: {
+        amount: -0.25,
+      },
+    });
+    tl.to(stairParent.current, {
+      display: "none",
+    });
+    tl.to(".stairs", {
+      y: "0%",
+    });
+    gsap.from(pageRef.current, {
+      opacity: 0,
+      scale: 1.3,
+      delay: 1.3,
+    });
+    gsap.to(pageRef.current, {
+      opacity: 1,
+      scale: 1,
+      delay: 1.3,
+    });
+  }, [currentLocation]);
+  return (
+    <div>
+      <div ref={stairParent} className="h-screen w-full fixed z-20 top-0">
+        <div className="h-full w-full flex">
+          <div className="stairs h-full w-1/6 bg-black"></div>
+          <div className="stairs h-full w-1/6 bg-black"></div>
+          <div className="stairs h-full w-1/6 bg-black"></div>
+          <div className="stairs h-full w-1/6 bg-black"></div>
+          <div className="stairs h-full w-1/6 bg-black"></div>
+          <div className="stairs h-full w-1/6 bg-black"></div>
+        </div>
+      </div>
+      <div ref={pageRef}>{children}</div>
+    </div>
+  );
+};
+
+export default Stairs;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,45 +1,12 @@
-import React, { useRef } from "react";
 import HomeHeroText from "../components/home/HomeHeroText";
 import Video from "../components/home/Video";
 import HomeBottomText from "../components/home/HomeBottomText";
-import { useGSAP } from "@gsap/react";
-import gsap from "gsap";
 
 const Home = () => {
-  const stairParent = useRef(null);
-  useGSAP(() => {
-    const tl = gsap.timeline();
-    tl.to(stairParent.current, {
-      display: "block",
-    });
-    tl.from(".stairs", {
-      height: 0,
-      stagger: {
-        amount: -0.3,
-      },
-    });
-    tl.to(".stairs", {
-      y: "100%",
-      stagger: {
-        amount: -0.3,
-      },
-    });
-    tl.to(stairParent.current, {
-      display: "none",
-    });
-  });
+
   return (
     <div>
-      <div ref={stairParent} className="h-screen w-full fixed z-20 top-0">
-        <div className="h-full w-full flex">
-          <div className="stairs h-full w-1/6 bg-black"></div>
-          <div className="stairs h-full w-1/6 bg-black"></div>
-          <div className="stairs h-full w-1/6 bg-black"></div>
-          <div className="stairs h-full w-1/6 bg-black"></div>
-          <div className="stairs h-full w-1/6 bg-black"></div>
-          <div className="stairs h-full w-1/6 bg-black"></div>
-        </div>
-      </div>
+    
       <div className="fixed h-[90vh] w-full flex justify-center">
         <HomeHeroText />
       </div>
